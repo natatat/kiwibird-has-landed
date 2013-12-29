1dcab74b000f8c88108de2cbbf70db1bf84b2183
natatat <natuduwela@gmail.com>
2013-12-28 19:20:02 -0800

    separate getNextListings function, except i think i maxed out the API

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
+var city,
+    lastPageHref,
+    nextPageHref,
+    totalPagesNum,
+    nextPageNum,
+    i;
+
+var listings = [];
+var airbnbListings = [];
+
+function getNextListings () {
+  $.ajax({
+    type: "GET",
+    url: "http://api.outpost.travel/placeRentals/?city=" + city + "&sortMethod=1&page=" + nextPageNum.toString(),
+  }).done(function(data){
+    nextPageHref = data._links.next.href;
+    nextPageNum = Math.floor(nextPageHref.split("&page=")[1]);
+
+    for ( i = 0; i < data._items.length; i++ ) {
+      listings.push(data._items[i]);
+    };
+  });
+
+  if ( nextPageNum <= totalPagesNum ) {
+    getNextListings();
+  } else {
+    for ( i = 0; i < listings.length; i++ ) {
+      if (listings[i].fullProvider == "Airbnb") {
+        // puts only airbnb listings in an array
+        airbnbListings.push(listings[i]);
+        return airbnbListings;
+
+        // start date? end date?
+        // for each listing, check the airbnb page itself for...
+        // "Minimum stay is 2 nights"
+        // "Those dates are not available"
+        // if includes, SKIP
+
+        // an array for all the listings with that availability
+      };
+    };
+  };
+};
+
 function getData () {
-  var city,
-      lastPageHref,
-      nextPageHref,
-      totalPagesNum,
-      nextPageNum,
-      i;
-
-  var listings = [];
-  var airbnbListings = [];
-
   city = "san%20francisco";
 
   $.ajax({
     type: "GET",
     url: "http://api.outpost.travel/placeRentals/?city=" + city,
   }).done(function(data){
+    console.log(data);
+    debugger
     lastPageHref = data._links.last.href;
     totalPagesNum = Math.floor(lastPageHref.split("&page=")[1]);
     nextPageHref = data._links.next.href;
@@ -22,42 +58,12 @@ function getData () {
 
     for ( i = 0; i < data._items.length; i++ ) {
       listings.push(data._items[i]);
-    }
+    };
 
-    // get this loop to work by redefining nextPageNum, calling a function recursively until nextPageNum <= totalPagesNum
-    if ( nextPageNum <= totalPagesNum ) {
-      $.ajax({
-        type: "GET",
-        url: "http://api.outpost.travel/placeRentals/?city=" + city + "&sortMethod=1&page=" + nextPageNum.toString(),
-      }).done(function(data){
-        nextPageHref = data._links.next.href;
-        nextPageNum = Math.floor(nextPageHref.split("&page=")[1]);
-
-        for ( i = 0; i < data._items.length; i++ ) {
-          listings.push(data._items[i]);
-        }
-      });
-    } else {
-      for ( i = 0; i < listings.length; i++ ) {
-        if (listings[i].fullProvider == "Airbnb") {
-          // puts only airbnb listings in an array
-          airbnbListings.push(listings[i])
-
-          // start date? end date?
-          // for each listing, check the airbnb page itself for...
-          // "Minimum stay is 2 nights"
-          // "Those dates are not available"
-          // if includes, SKIP
-
-          // an array for all the listings with that availability
-        };
-      }
-    }
-
-    debugger
+    getNextListings();
 
   });
-}
+};
 
 $(document).ready(function(){
   getData();
